228eb14c2f989dcf6b69b76a75875de6084cd2b3
Sebastian <info@sebastix.nl>
2026-04-13 10:57:11 +0200

    set accepting temp event to false

--- examples/default/main.go
+++ examples/default/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	//"context"
 	"net/http"
 	"os"
-	//"slices"
 
 	"github.com/fiatjaf/eventstore/sqlite3"
 	"github.com/fiatjaf/khatru"
 	"github.com/fiatjaf/khatru/policies"
-	//"github.com/nbd-wtf/go-nostr"
 )
 
 func main() {
@@ -18,10 +15,10 @@ func main() {
 
 	// NIP-11 info
 	relay.Info.Name = "relay.nosto.re"
-    relay.Info.PubKey = "npub1qe3e5wrvnsgpggtkytxteaqfprz0rgxr8c3l34kk3a9t7e2l3acslezefe"
-    relay.Info.Contact = "info@sebastix.nl"
-    relay.Info.Description = "Relay optimized for Blossom"
-    relay.Info.Version = "0.1"
+  relay.Info.PubKey = "npub1qe3e5wrvnsgpggtkytxteaqfprz0rgxr8c3l34kk3a9t7e2l3acslezefe"
+  relay.Info.Contact = "info@sebastix.nl"
+  relay.Info.Description = "Nostr relay optimized for NIP-B7 Blossom and NIP-5A Static Websites"
+  relay.Info.Version = "0.1"
 
 	db := sqlite3.SQLite3Backend{DatabaseURL: "./data/khatru-sqlite"}
 	os.MkdirAll("./data", 0755)
@@ -35,44 +32,29 @@ func main() {
 	relay.DeleteEvent = append(relay.DeleteEvent, db.DeleteEvent)
 
   allowedEventKinds := []uint16{24242,10063,34128,15128,35128}
-	relay.RejectEvent = append(relay.RejectEvent, policies.RestrictToSpecifiedKinds(true, allowedEventKinds[0]))
+  relay.RejectEvent = append(relay.RejectEvent, policies.RestrictToSpecifiedKinds(false, allowedEventKinds...))
 
-    // Custom policy
-    //relay.RejectEvent = append(relay.RejectEvent,
-    //    // We only accept events with kind 24242, 10063, 30563 so we put them in an array
-    //    func(ctx context.Context, event *nostr.Event) (reject bool, msg string) {
-    //        fmt.Printf("%T: %d \n", event.Kind, event.Kind)
-    //        slices.Sort(allowedEventKinds)
-    //        n, found := slices.BinarySearch(allowedEventKinds, uint16(event.Kind))
-    //        fmt.Println(n, found)
-    //        if found {
-    //            return false, ""
-    //        }
-    //        return true, "This event kind not allowed on this relay"
-    //    },
-    //)
-
-    // Output when there is HTTP request
-    mux := relay.Router()
-    // set up other http handlers
-    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("content-type", "text/html")
-        fmt.Fprintf(w, `<html><head></head><body>`)
-        fmt.Fprintf(w, `<div>`)
-        fmt.Fprintf(w, `<br /><br />`)
-        fmt.Fprintf(w, `This relay only accepts events with kinds:`)
-        fmt.Fprintf(w, `<ul>`)
-        fmt.Fprintf(w, `<li><code>24242</code> (Authorization event)</li>`)
-        fmt.Fprintf(w, `<li><code>10063</code> (User Blossom servers list event)</li>`)
-        fmt.Fprintf(w, `<li><code>34128</code> (nsite v1: static file event)</li>`)
-        fmt.Fprintf(w, `<li><code>15128</code> (nsite v2: root site manifest event)</li>`)
-        fmt.Fprintf(w, `<li><code>35128</code> (nsite v2: named site manifestl event)</li>`)
-        fmt.Fprintf(w, `</ul>`)
-        fmt.Fprintf(w, `<br /><br />`)
-        fmt.Fprintf(w, `<a href="https://github.com/Sebastix/khatru/tree/relay.nosto.re" target="https://github.com/Sebastix/khatru/tree/relay.nosto.re">https://github.com/Sebastix/khatru/tree/relay.nosto.re</a>`)
-        fmt.Fprintf(w, `</div>`)
-        fmt.Fprintf(w, `</body></html>`)
-    })
+  // Output when there is HTTP request
+  mux := relay.Router()
+  // set up other http handlers
+  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+      w.Header().Set("content-type", "text/html")
+      fmt.Fprintf(w, `<html><head></head><body>`)
+      fmt.Fprintf(w, `<div>`)
+      fmt.Fprintf(w, `<br /><br />`)
+      fmt.Fprintf(w, `This relay only accepts events with kinds:`)
+      fmt.Fprintf(w, `<ul>`)
+      fmt.Fprintf(w, `<li><code>24242</code> (Authorization event)</li>`)
+      fmt.Fprintf(w, `<li><code>10063</code> (User Blossom servers list event)</li>`)
+      fmt.Fprintf(w, `<li><code>34128</code> (nsite v1: static file event)</li>`)
+      fmt.Fprintf(w, `<li><code>15128</code> (nsite v2: root site manifest event)</li>`)
+      fmt.Fprintf(w, `<li><code>35128</code> (nsite v2: named site manifestl event)</li>`)
+      fmt.Fprintf(w, `</ul>`)
+      fmt.Fprintf(w, `<br /><br />`)
+      fmt.Fprintf(w, `<a href="https://github.com/Sebastix/khatru/tree/relay.nosto.re" target="https://github.com/Sebastix/khatru/tree/relay.nosto.re">https://github.com/Sebastix/khatru/tree/relay.nosto.re</a>`)
+      fmt.Fprintf(w, `</div>`)
+      fmt.Fprintf(w, `</body></html>`)
+  })
 
 	fmt.Println("running on :3338")
 	http.ListenAndServe(":3338", relay)
