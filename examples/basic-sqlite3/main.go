b118b96872235881e14701fcb9d1e794723c1377
Sebastian <info@sebastix.nl>
2026-04-13 11:04:06 +0200

    updates - block temp events

--- examples/basic-sqlite3/main.go
+++ examples/basic-sqlite3/main.go
@@ -31,50 +31,35 @@ func main() {
 	relay.ReplaceEvent = append(relay.ReplaceEvent, db.ReplaceEvent)
 
   allowedEventKinds := []uint16{0,3,5,1984,1985,1111,21,22,34235,34236,10040,30382,30383,30384,30385}
-	relay.RejectEvent = append(relay.RejectEvent, policies.RestrictToSpecifiedKinds(true, allowedEventKinds[0]))
+	relay.RejectEvent = append(relay.RejectEvent, policies.RestrictToSpecifiedKinds(false, allowedEventKinds...))
 
-    // Custom policy
-    //relay.RejectEvent = append(relay.RejectEvent,
-    //    // We only accept events with kind 37515, 13811 so we put them in an array
-    //    func(ctx context.Context, event *nostr.Event) (reject bool, msg string) {
-    //        fmt.Printf("%T: %d \n", event.Kind, event.Kind)
-    //        slices.Sort(allowedEventKinds)
-    //        n, found := slices.BinarySearch(allowedEventKinds, uint16(event.Kind))
-    //        fmt.Println(n, found)
-    //        if found {
-    //            return false, ""
-    //        }
-    //        return true, "This event kind not allowed on this relay"
-    //    },
-    //)
-
-    // Output when there is HTTP request
-    mux := relay.Router()
-    // set up other http handlers
-    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("content-type", "text/html")
-        fmt.Fprintf(w, `<html><head></head><body>`)
-        fmt.Fprintf(w, `<div style="text-align: center;">`)
-        fmt.Fprintf(w, `Connect your Nostr client to <code>wss://relay.kubo.watch</code>`)
-        fmt.Fprintf(w, `<br /><br />`)
-        fmt.Fprintf(w, `This relay only accepts events from authors: @todo`)
-        fmt.Fprintf(w, `<br /><br />`)
-        fmt.Fprintf(w, `This relay only accepts events with kind:`)
-        fmt.Fprintf(w, `<br />`)
-        fmt.Fprintf(w, `<code>0, 3, 5</code>`)
-        fmt.Fprintf(w, `<br />`)
-        fmt.Fprintf(w, `<code>1984, 1985</code>`)
-        fmt.Fprintf(w, `<br />`)
-        fmt.Fprintf(w, `<code>1111</code> (comment <a href="https://nips.nostr.com/22">NIP-22</a>)`)
-        fmt.Fprintf(w, `<br />`)
-        fmt.Fprintf(w, `<code>10040, 30382, 30383, 30384, 30385</code> (comment <a href="https://nips.nostr.com/85">NIP-85</a>)`)
-        fmt.Fprintf(w, `<br />`)
-        fmt.Fprintf(w, `<code>21</code>, <code>22</code>, <code>34235</code>, <code>34236</code> (comment <a href="https://nips.nostr.com/71">NIP-71</a>)`)
-        fmt.Fprintf(w, `<br /><br />`)
-        fmt.Fprintf(w, `<a href="https://github.com/nostrver-se/khatru/tree/relay.kubo.watch" target="_blank">https://github.com/nostrver-se/khatru/tree/relay.kubo.watch</a>`)
-        fmt.Fprintf(w, `</div>`)
-        fmt.Fprintf(w, `</body></html>`)
-    })
+  // Output when there is HTTP request
+  mux := relay.Router()
+  // set up other http handlers
+  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+      w.Header().Set("content-type", "text/html")
+      fmt.Fprintf(w, `<html><head></head><body>`)
+      fmt.Fprintf(w, `<div style="text-align: center;">`)
+      fmt.Fprintf(w, `Connect your Nostr client to <code>wss://relay.kubo.watch</code>`)
+      fmt.Fprintf(w, `<br /><br />`)
+      fmt.Fprintf(w, `This relay only accepts events from authors: @todo`)
+      fmt.Fprintf(w, `<br /><br />`)
+      fmt.Fprintf(w, `This relay only accepts events with kind:`)
+      fmt.Fprintf(w, `<br />`)
+      fmt.Fprintf(w, `<code>0, 3, 5</code>`)
+      fmt.Fprintf(w, `<br />`)
+      fmt.Fprintf(w, `<code>1984, 1985</code>`)
+      fmt.Fprintf(w, `<br />`)
+      fmt.Fprintf(w, `<code>1111</code> (comment <a href="https://nips.nostr.com/22">NIP-22</a>)`)
+      fmt.Fprintf(w, `<br />`)
+      fmt.Fprintf(w, `<code>10040, 30382, 30383, 30384, 30385</code> (comment <a href="https://nips.nostr.com/85">NIP-85</a>)`)
+      fmt.Fprintf(w, `<br />`)
+      fmt.Fprintf(w, `<code>21</code>, <code>22</code>, <code>34235</code>, <code>34236</code> (comment <a href="https://nips.nostr.com/71">NIP-71</a>)`)
+      fmt.Fprintf(w, `<br /><br />`)
+      fmt.Fprintf(w, `<a href="https://github.com/nostrver-se/khatru/tree/relay.kubo.watch" target="_blank">https://github.com/nostrver-se/khatru/tree/relay.kubo.watch</a>`)
+      fmt.Fprintf(w, `</div>`)
+      fmt.Fprintf(w, `</body></html>`)
+  })
 
 	fmt.Println("running on :1212")
 	http.ListenAndServe(":1212", relay)
